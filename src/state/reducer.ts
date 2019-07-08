9ff90a7c86231b2062b9a907b1a37046cbb549f0
Shannon Oram <shannon.oram@gmail.com>
2019-07-08 16:23:14 +1000

    Add reducer implementation for remaining commands

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -2,13 +2,14 @@ import { State } from './state';
 import { Command } from '../types/commands';
 import { UnreachableCaseError } from '../utils/unreachable-case-error';
 import { Table } from '../types/table';
+import { EntityPosition } from '../types/entity';
 
 
-// The reducer function is simplified example of what might be used in a "real" application.
+// The reducer function is simplified example of what might be used in a real application.
 //
 // 1. The `Command` type here only represents user initiated actions, which obviously wouldn't
 //    be the case in a real application. Therefore, I would replace the `Command` type used
-//    here with a new `Action` type.
+//    here with a new `Action` type. User commands would be a subset of possible actions.
 //
 // 2. The `state` value is an immutable value. I would consider using "Immer" to  ensure the
 //    `state` wasn't inadvertently modified.
@@ -42,15 +43,49 @@ export const reduce = (state: State, command: Command): State => {
         }
 
         case 'MOVE_FORWARD': {
-            return state;
+            if (state.robot.position === null) {
+                return state;
+            } else {
+                const newPosition = EntityPosition.moveForward(state.robot.position);
+                if (Table.isPositionInsideBounds(state.table, newPosition.x, newPosition.y)) {
+                    return {
+                        ...state,
+                        robot: {
+                            position: newPosition,
+                        },
+                    };
+                } else {
+                    return state;
+                }
+            }
         }
 
         case 'ROTATE_LEFT': {
-            return state;
+            if (state.robot.position === null) {
+                return state;
+            } else {
+                const newPosition = EntityPosition.rotateLeft(state.robot.position);
+                return {
+                    ...state,
+                    robot: {
+                        position: newPosition,
+                    },
+                };
+            }
         }
 
         case 'ROTATE_RIGHT': {
-            return state;
+            if (state.robot.position === null) {
+                return state;
+            } else {
+                const newPosition = EntityPosition.rotateRight(state.robot.position);
+                return {
+                    ...state,
+                    robot: {
+                        position: newPosition,
+                    },
+                };
+            }
         }
 
         case 'REPORT_POSITION': {
--- src/types/entity.ts
+++ src/types/entity.ts
@@ -1,7 +1,44 @@
+import { UnreachableCaseError } from '../utils/unreachable-case-error';
+
 export type EntityDirection = 'NORTH' | 'SOUTH' | 'EAST' | 'WEST';
 
 export interface EntityPosition {
     x: number;
     y: number;
     facing: EntityDirection;
+}
+
+export namespace EntityPosition {
+    export function moveForward(current: EntityPosition): EntityPosition {
+        switch (current.facing) {
+            case 'NORTH': return { ...current, y: current.y + 1};
+            case 'SOUTH': return { ...current, y: current.y - 1 };
+            case 'EAST': return { ...current, x: current.x + 1 };
+            case 'WEST': return { ...current, x: current.x - 1 };
+            default:
+                throw new UnreachableCaseError(current.facing);
+        }
+    }
+
+    export function rotateLeft(current: EntityPosition): EntityPosition {
+        switch (current.facing) {
+            case 'NORTH': return { ...current, facing: 'WEST' };
+            case 'SOUTH': return { ...current, facing: 'EAST' };
+            case 'EAST': return { ...current, facing: 'NORTH' };
+            case 'WEST': return { ...current, facing: 'SOUTH' };
+            default:
+                throw new UnreachableCaseError(current.facing);
+        }
+    }
+
+    export function rotateRight(current: EntityPosition): EntityPosition {
+        switch (current.facing) {
+            case 'NORTH': return { ...current, facing: 'EAST' };
+            case 'SOUTH': return { ...current, facing: 'WEST' };
+            case 'EAST': return { ...current, facing: 'SOUTH' };
+            case 'WEST': return { ...current, facing: 'NORTH' };
+            default:
+                throw new UnreachableCaseError(current.facing);
+        }
+    }
 }
