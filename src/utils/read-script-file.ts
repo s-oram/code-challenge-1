787480fd2b32d82199c7a9366b7b2e495d4e26af
Shannon Oram <shannon.oram@gmail.com>
2019-07-08 17:10:23 +1000

    Add readScriptFile() implementation

--- src/utils/read-script-file.ts
+++ src/utils/read-script-file.ts
@@ -1,8 +1,9 @@
-// import fs from 'fs';
+import fs from 'fs';
 
 export function readScriptFile(filename: string): string[] {
-    return [];
+    const buffer = fs.readFileSync(filename);
+    // Split the string using on the new line character. The regex is to support
+    // both Windows and UNIX style line endings.
+    // https://stackoverflow.com/a/21895354/395461
+    return buffer.toString().split(/\r?\n/);
 }
-
-
-
