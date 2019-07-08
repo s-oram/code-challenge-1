2c2b251f60d3b2c5627466ec518a60241f91689b
Shannon Oram <shannon.oram@gmail.com>
2019-07-08 14:32:18 +1000

    Add reducer skeleton

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -1,6 +1,27 @@
 import { State } from './state';
 import { Command } from '../types/commands';
+import { UnreachableCaseError } from '../utils/unreachable-case-error';
 
 export const reduce = (state: State, command: Command): State => {
-    return state;
+
+    switch (command.type) {
+        case 'PLACE':
+            return state;
+
+        case 'MOVE_FORWARD':
+            return state;
+
+        case 'ROTATE_LEFT':
+            return state;
+
+        case 'ROTATE_RIGHT':
+            return state;
+
+        case 'REPORT_POSITION':
+            return state;
+
+        default:
+            throw new UnreachableCaseError(command);
+    }
+
 };
